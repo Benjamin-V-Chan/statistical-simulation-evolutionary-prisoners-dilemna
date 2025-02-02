260caee199ae8e98a8bc9f4b3cd21783713e743d
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-02 00:03:54 -0800

    delete 05

--- scripts/05_run_full_simulation.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("python scripts/01_initialize_population.py")
-for _ in range(10):  # 10 generations
-    os.system("python scripts/02_run_tournament.py")
-    os.system("python scripts/03_evolve_population.py")
-os.system("python scripts/04_visualize_results.py")
